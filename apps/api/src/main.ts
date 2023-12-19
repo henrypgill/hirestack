8150cf9c397f3b4e0bd5b852fad7c0ef084c2434
henrypgill <hpgill312@gmail.com>
2023-12-19 13:54:38 +0000

    added logging to startup script

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import dotenv from "dotenv";
+
+import { PrismaClient } from "@prisma/client";
+
+// dotenv.config();
+
+export const database = new PrismaClient();
+
 
 async function bootstrap() {
+  console.log("Connecting to database...");
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  console.log("Connected to database");
+  // const port = process.env.PORT || 4000;
+  const port = 4000;
+  await app.listen(port);
+  console.log("Listening on port: " + port);
 }
-bootstrap();
+bootstrap()
+.catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+});
