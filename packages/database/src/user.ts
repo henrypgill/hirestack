a0fc293ec45ff31db578883558ec8984fddab552
henrypgill <hpgill312@gmail.com>
2023-12-19 13:54:11 +0000

    created type for a FullUser

---
+++ packages/database/src/user.ts
@@ -0,0 +1,23 @@
+import { Prisma } from "@prisma/client";
+
+
+export type FullUser = Prisma.UserGetPayload<FullUserArgs>
+export type FullUserArgs = ReturnType<typeof fullUserArgs>
+export function fullUserArgs() {
+    return {
+        select: {
+            id: true as true,
+            name: true as true,
+            pronouns: true as true,
+            jobTitle: true as true,
+            location: true as true,
+            email: true as true,
+            phone: true as true,
+            linkedin: true as true,
+            github: true as true,
+            website: true as true,
+            bio: true as true,
+        },
+        include: {education: true as true, skills: true as true, roles: true as true}
+    }
+}
