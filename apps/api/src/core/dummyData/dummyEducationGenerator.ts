ee7c9b924345ebdd556a3c8da6a738831f95310b
henrypgill <hpgill312@gmail.com>
2023-12-19 13:50:53 +0000

    created files and functions to generate dummy data

---
+++ apps/api/src/core/dummyData/dummyEducationGenerator.ts
@@ -0,0 +1,60 @@
+
+import { Prisma } from "@hirestack/database"
+import { generateLoremIpsum, randomArrayElement } from "../utils";
+
+
+export function dummyManyEducationGenerator(count): Prisma.EducationCreateWithoutUserInput[] {
+    const education:  Prisma.EducationCreateWithoutUserInput[] = []
+    for (let i = 0; i < count; i++) {
+        education.push(dummyEducationGenerator())
+    }
+    return education
+}
+
+
+export function dummyEducationGenerator(): Prisma.EducationCreateWithoutUserInput {
+    const randomSpecialisation = () => randomArrayElement(["Computer Science", "Electrical Engineering", "Mechanical Engineering", "Biomedical Engineering", "Chemical Engineering", "Civil Engineering", "Aerospace Engineering", "Software Engineering", "Mathematics", "Physics", "Chemistry", "Biology", "Environmental Science", "Information Technology", "Data Science", "Astrophysics", "Materials Science", "Geology", "Statistics", "Robotics Engineering"]);
+    
+    const randomProvider = () => randomArrayElement([
+        "University of Cambridge",
+        "Imperial College London",
+        "University of Oxford",
+        "University College London (UCL)",
+        "University of Edinburgh",
+        "University of Manchester",
+        "University of Bristol",
+        "University of Warwick",
+        "University of Glasgow",
+        "University of Southampton",
+        "University of Birmingham",
+        "University of Leeds",
+        "University of Sheffield",
+        "University of Nottingham",
+        "University of York",
+        "King's College London",
+        "Queen Mary University of London",
+        "University of Liverpool",
+        "University of Bath",
+        "Lancaster University",
+        "Le Wagon London",
+        "General Assembly London",
+        "Makers Academy London",
+        "Flatiron School London",
+        "Northcoders Manchester",
+        "Tech Talent Academy Edinburgh",
+        "CodeClan Glasgow",
+        "School of Code Birmingham",
+        "DevelopMe Bristol",
+        "Cyber Retraining Academy London"
+      ]);
+
+    const randomType = () => randomArrayElement(["bachelors", "masters", "phd", "bootcamp", "course"])
+    
+    return {
+        specialization: randomSpecialisation(),
+        provider: randomProvider(),
+        type: randomType(),
+        description: generateLoremIpsum(100)
+    }
+}
+
---
+++ apps/api/src/core/dummyData/dummySkillGenerator.ts
@@ -0,0 +1,59 @@
+
+import { Prisma } from "@hirestack/database"
+import { generateLoremIpsum, randomArrayElement } from "../utils";
+
+
+
+export function dummyManySkillGenerator(count): Prisma.SkillCreateWithoutUserInput[] {
+    const skills:  Prisma.SkillCreateWithoutUserInput[] = []
+    for (let i = 0; i < count; i++) {
+        skills.push(dummySkillGenerator())
+    }
+    return skills
+}
+
+export function dummySkillGenerator(): Prisma.SkillCreateWithoutUserInput {
+    
+    const randomName = () => randomArrayElement([
+        "University of Cambridge",
+        "Imperial College London",
+        "University of Oxford",
+        "University College London (UCL)",
+        "University of Edinburgh",
+        "University of Manchester",
+        "University of Bristol",
+        "University of Warwick",
+        "University of Glasgow",
+        "University of Southampton",
+        "University of Birmingham",
+        "University of Leeds",
+        "University of Sheffield",
+        "University of Nottingham",
+        "University of York",
+        "King's College London",
+        "Queen Mary University of London",
+        "University of Liverpool",
+        "University of Bath",
+        "Lancaster University",
+        "Le Wagon London",
+        "General Assembly London",
+        "Makers Academy London",
+        "Flatiron School London",
+        "Northcoders Manchester",
+        "Tech Talent Academy Edinburgh",
+        "CodeClan Glasgow",
+        "School of Code Birmingham",
+        "DevelopMe Bristol",
+        "Cyber Retraining Academy London"
+      ]);
+
+    const randomType = () => randomArrayElement(["language", "technology", "software", "other"])
+    
+    return {
+        name: randomName(),
+        type: randomType(),
+        experience: Math.floor(Math.random() * 10),
+        description: generateLoremIpsum(50),
+    }
+}
+
