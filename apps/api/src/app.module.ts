b5352ec1bffda3271e671226b57a812c6852bc04
henrypgill <hpgill312@gmail.com>
2023-12-19 13:52:35 +0000

    added user and util modules to root nest module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './users/user.module';
+import { DbUtilsModule } from './dbUtils/dbUtils.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, DbUtilsModule],
   controllers: [AppController],
   providers: [AppService],
 })
