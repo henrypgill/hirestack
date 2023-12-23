a60f2c94e6b13499786ad78f6e086178d067e306
henrypgill <hpgill312@gmail.com>
2023-12-23 13:37:25 +0000

    added endpoint for getting a filtered list of users

--- apps/api/src/users/user.controller.ts
+++ apps/api/src/users/user.controller.ts
@@ -2,18 +2,24 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { UserService } from './user.service.js';
 import { FullUser } from '@hirestack/database';
 
-@Controller()
+@Controller("/users")
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get("/users")
+  @Get("/")
   async getAllUsers(): Promise<FullUser[]> {
     const users = await this.userService.getAll();
     return users
   }
 
-  @Get("/users/:userId")
+  @Get("/:userId")
   async getUser(@Param() params: {userId: string}): Promise<FullUser> {
+    const user = await this.userService.getUnique(params.userId);
+    return user
+  }
+
+  @Get("/:userId")
+  async getFilteredUsers(@Param() params: {userId: string}): Promise<FullUser> {
     const user = await this.userService.getUnique(params.userId);
     return user
   }
