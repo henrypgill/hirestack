484084a73dc1e53a955c285c741b51c5ae7bed2a
henrypgill <hpgill312@gmail.com>
2023-12-27 18:52:39 +0000

    types for filtering

--- packages/database/src/user.ts
+++ packages/database/src/user.ts
@@ -1,64 +1,56 @@
-import { Prisma, Skill } from "@prisma/client"
-import { EducationFilter, SkillFilter } from "./types";
+import { Prisma } from "@prisma/client";
 
-
-export type FullUser = Prisma.UserGetPayload<FullUserArgs>
-export type FullUserArgs = ReturnType<typeof fullUserArgs>
+export type FullUser = Prisma.UserGetPayload<FullUserArgs>;
+export type FullUserArgs = ReturnType<typeof fullUserArgs>;
 export function fullUserArgs() {
-    return {
-        select: {
-            id: true as true,
-            name: true as true,
-            pronouns: true as true,
-            jobTitle: true as true,
-            location: true as true,
-            email: true as true,
-            phone: true as true,
-            linkedin: true as true,
-            github: true as true,
-            website: true as true,
-            bio: true as true,
-            education: true as true,
-            skills: true as true,
-            roles: true as true
-        },
-    }
+  return {
+    select: {
+      id: true as true,
+      name: true as true,
+      pronouns: true as true,
+      jobTitle: true as true,
+      location: true as true,
+      email: true as true,
+      phone: true as true,
+      linkedin: true as true,
+      github: true as true,
+      website: true as true,
+      bio: true as true,
+      education: true as true,
+      skills: true as true,
+      roles: true as true,
+    },
+  };
 }
 
-type A = Skill
-
-export interface UserFilterArgs {
-    education: EducationFilter[];
-    skills: SkillFilter[];
-    experience?: number;
-  }
-
-export function getFilterUserArgs({education, skills, experience}: UserFilterArgs): Prisma.UserFindManyArgs {
-      
-    const AndArgs: Prisma.UserWhereInput[] = []
-
-    skills.forEach(skill => {
-        AndArgs.push({
-            AND: [
-                {
-                    skills: {
-                        some: {
-                            experience: {
-                                gte: skill.experience
-                            }
-                        }
-                }
-            }
-        ]
-        })
-    })
-
-    return {
-        ...fullUserArgs(),
-        where: {
-            OR: [
-
-            ]
-        }
-    }
+export interface SkillFilter {
+  name: string;
+  type: string;
+  experience: number;
 }
+
+export interface EducationFilter {
+  specialization: string;
+  type: string;
+  provider: string;
+}
+
+export interface UserFilter {
+  education: EducationFilter[];
+  skills: SkillFilter[];
+  experience?: number;
+}
+
+// export function filterUsersBySkillArgs({name, type, experience}: SkillFilterArgs): Prisma.SkillWhereInput {
+
+//     return {
+//         AND: [
+//             {
+//                 name,
+//                 type,
+//                 experience,
+//             },
+//     ]
+//     }
+
+// }
