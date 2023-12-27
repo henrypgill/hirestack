6c0e708a55118350d78e6b63a58563766bed2b67
henrypgill <hpgill312@gmail.com>
2023-12-27 20:49:59 +0000

    Created type for User and isolated related types

---
+++ packages/database/src/User.ts
@@ -0,0 +1,30 @@
+import { Education, EducationFilter } from "./Education"
+import { Role } from "./Role"
+import { Skill, SkillFilter } from "./Skill"
+
+export interface User {
+  _id: string
+  name: string
+  pronouns: string
+  jobTitle: string
+  location: string
+  email: string
+  phone: string
+  linkedin: string
+  github: string
+  website: string
+  bio: string
+  education: Education[],
+  skills: Skill[];
+  roles: Role[];
+}
+
+
+
+
+
+export interface UserFilter {
+  education: EducationFilter[];
+  skills: SkillFilter[];
+  experience?: number;
+}
