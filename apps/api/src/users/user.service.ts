88ca0fd00f18c6e70ea3abe60353f0a9f930f2a3
henrypgill <hpgill312@gmail.com>
2023-12-27 18:52:20 +0000

    wrote some more filtering logic

--- apps/api/src/users/user.service.ts
+++ apps/api/src/users/user.service.ts
@@ -1,38 +1,43 @@
-import { EducationFilter, EducationLevel, FullUser, FullUserArgs, SkillFilter, UserFilterArgs, fullUserArgs } from '@hirestack/database';
-import { Injectable } from '@nestjs/common';
-import { database } from '../main.js';
-
-
+import { FullUser, FullUserArgs, fullUserArgs } from "@hirestack/database";
+import { Injectable } from "@nestjs/common";
+import { database } from "../main.js";
+import { parseFilterString } from "./filter.js";
 
 @Injectable()
 export class UserService {
-
   async getAll(): Promise<FullUser[]> {
     const users = await database.user.findMany<FullUserArgs>(fullUserArgs());
-    return users
+    return users;
   }
 
   async getUnique(userId: string): Promise<FullUser> {
     const user = await database.user.findUnique({
       where: {
-        id: userId
+        id: userId,
       },
-      ...fullUserArgs()
-  });
-    return user
+      ...fullUserArgs(),
+    });
+    return user;
   }
 
-
-  async getFilteredUsers(filterArgs: UserFilterArgs): Promise<FullUser[]> {
-
-
+  async getFiltered(filterString: string): Promise<FullUser[]> {
+    const { skills, education, experience } = parseFilterString(filterString);
+    const skillsFilter = skills.map((skillFilter) => {
+      return {};
+    });
 
     const users = await database.user.findMany({
       ...fullUserArgs(),
       where: {
+        skills: {
+          every: {
+            name: "University of Leeds",
+            type: "language",
+          },
+        },
+      },
+    });
 
-      }
-    })
+    return users;
   }
-
 }
