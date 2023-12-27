a16b2d8fe90b1af08f8764ef82af95df95b61edf
henrypgill <hpgill312@gmail.com>
2023-12-27 18:51:53 +0000

    changed route paths

--- apps/api/src/users/user.controller.ts
+++ apps/api/src/users/user.controller.ts
@@ -1,26 +1,32 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { UserService } from './user.service.js';
-import { FullUser } from '@hirestack/database';
+import { Controller, Get, Param } from "@nestjs/common";
+import { UserService } from "./user.service.js";
+import { FullUser } from "@hirestack/database";
+import { parseFilterString } from "./filter.js";
 
 @Controller("/users")
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get("/")
+  @Get("/all")
   async getAllUsers(): Promise<FullUser[]> {
     const users = await this.userService.getAll();
-    return users
+    return users;
   }
 
-  @Get("/:userId")
-  async getUser(@Param() params: {userId: string}): Promise<FullUser> {
+  @Get("/unique/:userId")
+  async getUser(@Param() params: { userId: string }): Promise<FullUser> {
     const user = await this.userService.getUnique(params.userId);
-    return user
+    return user;
   }
 
-  @Get("/:userId")
-  async getFilteredUsers(@Param() params: {userId: string}): Promise<FullUser> {
-    const user = await this.userService.getUnique(params.userId);
-    return user
+  @Get("/filter/:filters")
+  async getFilteredUsers(@Param() params: { filters: string }) {
+    //: Promise<FullUser[]> {
+    console.log(
+      "============================================================================",
+    );
+    const user = await this.userService.getFiltered(params.filters);
+    return user;
+    return parseFilterString(params.filters);
   }
-} 
+}
