c3a6ff9b1e85310a4c160b994e40ca6c53bbed82
henrypgill <hpgill312@gmail.com>
2023-12-27 20:51:20 +0000

    removed prisma client and replaced with mongodb client

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,23 +1,35 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module.js";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 
-import { PrismaClient } from "@prisma/client";
+import { MongoClient } from "mongodb";
 
-// dotenv.config();
 
-export const database = new PrismaClient();
+export const database = new MongoClient(process.env.DATABASE_URL)
 
 async function bootstrap() {
+
   console.log("Connecting to database...");
-  const app = await NestFactory.create(AppModule);
+  await database.connect();
   console.log("Connected to database");
-  // const port = process.env.PORT || 4000;
-  const port = 4000;
+
+
+  console.log("Starting up Nest Service");
+  const app = await NestFactory.create(AppModule);
+  console.log("Nest service started, API ready to go");
+
+
+
+
+  console.log("Opening connection... ... ...");
+  
+  const port = process.env.PORT || 4000;
+  // const port = 4000;
   await app.listen(port);
-  console.log("Listening on port: " + port);
+  console.log("Connection open. Listening on port: " + port);
 }
 bootstrap().catch(async (e) => {
   console.error(e);
+  await database.close()
   process.exit(1);
 });
